8919d57049ed5711f0f8d297e0ee22567b0e7c30
Adrianlsx <145877144+Adrianlsx@users.noreply.github.com>
2024-10-20 06:22:41 +0800

    Update sim.js

--- cmds/sim.js
+++ cmds/sim.js
@@ -2,30 +2,36 @@ const axios = require("axios");
 
 module.exports = {
     name: "sim",
-    info: "Chat with Simini",
-    dev: "Eugene Aguilar",
+    info: "Talk to Sim",
+    dev: "KENLIEPLAYS",
     onPrefix: true, // Change to false if you want no prefix
     dmUser: false, // Change to true if you want to allow DMs
-    nickName: ["simsimi"],
-    usages: "{pn} [message]",
-    cooldowns: 3, // cooldown time in seconds
+    nickName: [],
+    usages: "[ask]",
+    cooldowns: 2, // Cooldown time in seconds
 
     onLaunch: async function ({ api, event, actions, args }) {
+        const { messageID, threadID, senderID } = event;
+        const tid = threadID;
+        const mid = messageID;
+        const content = encodeURIComponent(args.join(" "));
+
+        if (!args[0]) {
+            return actions.reply("Please type a message...");
+        }
+
         try {
-            let message = args.join(" ");
-            if (!message) {
-                return actions.reply("Please put a message.");
+            const res = await axios.get(`https://simsimi.site/api/v2/?mode=talk&lang=ph&message=${content}&filter=false`);
+            const respond = res.data.success;
+
+            if (res.data.error) {
+                await actions.reply(`Error: ${res.data.error}`);
+            } else {
+                await actions.reply(respond);
             }
-
-            console.log(`Sending request to API with message: ${message}`);
-            const response = await axios.get(`https://markdevs69v2.onrender.com/api/sim/get/${message}`);
-            const respond = response.data.reply;
-
-            console.log(`Sending message to user: ${respond}`);
-            await actions.reply(respond);
         } catch (error) {
-            console.error("An error occurred:", error);
-            await actions.reply("Oops! Something went wrong.");
+            console.error(error);
+            await actions.reply("An error occurred while fetching the data.");
         }
     }
 };
